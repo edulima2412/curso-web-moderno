3ab87fbb4a7fa74116437ee2c1cdb834f6dbba73
Eduardo Lima <edulima2412@gmail.com>
2019-04-19 13:29:31 -0300

    conclusao de fundamentos

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+// Operadores binarios
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+// Operador unario
+// -a
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
